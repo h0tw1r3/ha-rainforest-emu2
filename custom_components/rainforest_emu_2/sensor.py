fd688c2a32ba3f5e6b8f1bc6ed45170b171188f3
Damien Heiser <damienheiser@users.noreply.github.com>
2020-10-20 19:51:03 -0700

    user defined price support
    * Added the ability to set a user defined price, and have that sent to the EMU2
    * Add helper input_number.rainforest_tariff_custom_rate

--- custom_components/rainforest_emu_2/sensor.py
+++ custom_components/rainforest_emu_2/sensor.py
@@ -13,7 +13,7 @@ import logging
 import voluptuous as vol
 from threading import Thread
 
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -29,6 +29,7 @@ ATTR_PRICE = "Price"
 ATTR_SUMMATION = "Net kWh"
 ATTR_DELIVERED = "Delivered kWh"
 ATTR_RECEIVED = "Received kWh"
+ATTR_CUSTOMPRICE = "Custom Price"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_PORT): cv.string,
@@ -40,7 +41,7 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     port = config.get(CONF_PORT)
     sensor_name = config.get(CONF_NAME)
 
-    sensor = EMU2Sensor(sensor_name, port)
+    sensor = EMU2Sensor(sensor_name, port, hass)
 
     hass.bus.async_listen_once(
         EVENT_HOMEASSISTANT_STOP, sensor.stop_serial_read())
@@ -48,8 +49,9 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     async_add_entities([sensor])
 
 class EMU2Sensor(Entity):
-    def __init__(self, sensor_name, port):
+    def __init__(self, sensor_name, port, hass):
         _LOGGER.debug("Init")
+        self._hass = hass
         self._port = port
         self._name = sensor_name
         self._baudrate = 115200
@@ -70,6 +72,11 @@ class EMU2Sensor(Entity):
         self._data[ATTR_SUMMATION] = None
         self._data[ATTR_DELIVERED] = None
         self._data[ATTR_RECEIVED] = None
+        custom_price = self._hass.states.get('input_number.rainforest_tariff_custom_rate')
+        if custom_price is not None:
+            self._data[ATTR_CUSTOMPRICE] = custom_price.state
+        else:
+            self._data[ATTR_CUSTOMPRICE] = None
 
     @property
     def name(self):
@@ -85,6 +92,7 @@ class EMU2Sensor(Entity):
             ATTR_SUMMATION: self._data.get(ATTR_SUMMATION),
             ATTR_DELIVERED: self._data.get(ATTR_DELIVERED),
             ATTR_RECEIVED: self._data.get(ATTR_RECEIVED),
+            ATTR_CUSTOMPRICE: self._data.get(ATTR_CUSTOMPRICE),
         }
         
     @property
@@ -151,6 +159,19 @@ class EMU2Sensor(Entity):
                         self.async_schedule_update_ha_state()
                         
                         _LOGGER.debug("InstantaneousDemand: %s", self._state)
+                        
+                        custom_price = self._hass.states.get('input_number.rainforest_tariff_custom_rate')
+                        if custom_price is not None:
+                            self._data[ATTR_CUSTOMPRICE] = custom_price.state
+                        
+                        if self._data[ATTR_CUSTOMPRICE] is not None:
+                            self._data[ATTR_PRICE] = self._data[ATTR_CUSTOMPRICE]
+                            priceHex = hex(int(round(float(self._data[ATTR_CUSTOMPRICE]))))
+                            
+                            setPriceCommand='<Command><Name>set_current_price</Name><Price>'+priceHex+'</Price><TrailingDigits>0x05</TrailingDigits></Command>'
+                            _LOGGER.debug ('setPriceCommand: %s', setPriceCommand)
+                            commandResult = reader.write (str.encode(setPriceCommand))
+                            _LOGGER.debug ('commandResult: %s', commandResult)
 
                     elif xmlTree.tag == 'PriceCluster':
                         priceRaw = int(xmlTree.find('Price').text, 16)
